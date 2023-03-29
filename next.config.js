e0adeb8ea74e7fc81fa195516f120c6246ca1d9e
mastereater <mastereaterwebsite@gmail.com>
2023-03-29 18:55:13 +0700

    added ga again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    googleAnalyticsID: process.env.googleAnalyticsID,
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "./globals.css";
 import NextNProgress from "nextjs-progressbar";
 import { GoogleAnalytics } from "nextjs-google-analytics";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.googleAnalyticsID) {
+      // Checks for GA ID and only turns on GA in production
+      ReactGA.initialize(process.env.googleAnalyticsID);
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  });
   return (
     <>
       <NextNProgress
